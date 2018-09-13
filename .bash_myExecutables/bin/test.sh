6228e9f57a59d9da0584f160302537346c09769a
Jason Kralik <jgkra123@gmail.com>
2018-09-12 20:37:18 -0400

    Cocoapods added to Darwin

---
+++ .bash_myExecutables/bin/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#############################################
+
+# Author: Jason Kralik
+# Purpose: 
+#  
+# FileName: test.sh
+# Created: 08/24/18 
+# Modifed: 
+
+#############################################
+
+main() {
+	echo "hello"
+}
+
+main() $@
